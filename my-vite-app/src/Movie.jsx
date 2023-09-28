4c0ce0207c17a362476b108e8469e6a77e7a5b2d
Your Name <me@me.com>
2023-09-28 12:40:49 -0500

    Code Review

--- my-vite-app/src/Movie.jsx
+++ my-vite-app/src/Movie.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-
+import  { useState } from 'react';
+// LOOKUP PROP VALIDATION IN VITE APP or go in the .eslintrc.cjs file and change the rule to "off"
 const Movie = ({ movie }) => {
   const [reviewText, setReviewText] = useState('');
   const [rating, setRating] = useState(0);
@@ -21,15 +21,18 @@ const Movie = ({ movie }) => {
     setReviewText('');
     setRating(0);
   };
-
+// recopied the src path and it worked, not sure the difference looked over it for a while and no clue why... -mv
   return (
-    <div className="movie">
-      <img src={`https://image.tmbd.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-      <div className="movie-info">
+    <div className='movie'>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <div className='movie-info'>
         <h3>{movie.title}</h3>
         <span>{movie.vote_average}</span>
       </div>
-      <div className="movie-over">
+      <div className='movie-over'>
         <h2>Overview:</h2>
         <p>{movie.overview}</p>
       </div>
@@ -37,7 +40,7 @@ const Movie = ({ movie }) => {
         {[1, 2, 3, 4, 5].map((star) => (
           <span
             key={star}
-            className={`star ${star <= rating ? 'active' : ''}`}
+            className={`star ${star <= rating ? "active" : ""}`}
             onClick={() => setRating(star)}
           >
             ★
@@ -46,7 +49,7 @@ const Movie = ({ movie }) => {
       </div>
       <div>
         <textarea
-          placeholder="Leave a review"
+          placeholder='Leave a review'
           value={reviewText}
           onChange={(e) => setReviewText(e.target.value)}
         />
